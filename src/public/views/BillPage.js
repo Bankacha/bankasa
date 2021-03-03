9ec158dbddd2e303009e3eeb87aba5901d6a7cab
Veljko Stanojevic <stveljko012@gmail.com>
2021-03-03 10:28:23 +0100

    color

--- src/public/views/BillPage.js
+++ src/public/views/BillPage.js
@@ -8,15 +8,22 @@ export function BillPage() {
         <Row className='bg-light h-100'>
             <Col xs={8}>
                 <Row className="h-25 bg-warning pb-3">
-                    <CategoryButton>Pice</CategoryButton>
-                    <CategoryButton>Pice</CategoryButton>
-                    <CategoryButton>Pice</CategoryButton>
-                    <CategoryButton>Pice</CategoryButton>
-                    <CategoryButton>Pice</CategoryButton>
-                    <CategoryButton>Pice</CategoryButton>
-                </Row>
-                <Row className="position-absolute h-75 bg-success overflow-auto w-100 pt-3">
                     <Col>
+                        <Row className="h-50">
+                            
+                        </Row>
+                        <Row className="h-50">
+                            <CategoryButton>Pice</CategoryButton>
+                            <CategoryButton>Pice</CategoryButton>
+                            <CategoryButton>Pice</CategoryButton>
+                            <CategoryButton>Pice</CategoryButton>
+                            <CategoryButton>Pice</CategoryButton>
+                            <CategoryButton>Pice</CategoryButton>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row className="position-absolute h-75 overflow-auto w-100">
+                    <Col className="bg-light pt-4">
                         <Row className="justify-content-center">
                             <ProductCard name="title" price={350.00}/>
                             <ProductCard name="title" price={350.00}/>
