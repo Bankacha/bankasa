16f1c245384e0162cf97b6aa4b8060dcb52bd54a
Your Name <bgbrankoman@gmail.com>
2021-03-31 15:59:39 +0200

    render waiter name and current time in orderItem header

--- src/public/components/bill-create/sub-components/order-component/OrderItemsHeader.js
+++ src/public/components/bill-create/sub-components/order-component/OrderItemsHeader.js
@@ -1,11 +1,22 @@
+import { useState } from "react"
 import { Col } from "react-bootstrap"
+import { useSelector } from "react-redux"
+import { getCurrentUser } from "../../../../../store/selectors"
 
 export function OrderItemsHeader() {
+    
+    const currentUser = useSelector(getCurrentUser)
+    const [time , setTime] = useState(new Date())
+
+
+    setTimeout(()=> setTime(new Date()), 55000)
+
+    const [h , m , s] = `${time}`.split(' ')[4].split(':')
 
     return (
         <Col className="text-light my-1 px-4 d-flex justify-content-between">
-            <h6>Waiter Name</h6>
-            <h6>08:02</h6>
+            <h6>{currentUser.name}</h6>
+            <h6>{`${h}:${m}`}</h6>
         </Col>
     )
 }
