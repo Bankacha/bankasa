30b536859a60fba1b5683cfb1d7717580bfab8e7
Your Name <bgbrankoman@gmail.com>
2021-04-01 10:08:39 +0200

    dont charge bill unless there are billItems

--- src/public/components/bill-create/sub-components/order-component/OrderComponentButtons.js
+++ src/public/components/bill-create/sub-components/order-component/OrderComponentButtons.js
@@ -2,18 +2,25 @@ import { Row, Col, Button } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
 import { reduceStock, saveAndPrintOrder } from "../../../../../store/actions"
 import { chargeBill } from "../../../../../store/actions"
-import { getCurrentUser, getOrderItems } from "../../../../../store/selectors"
+import { getBillItems, getCurrentUser, getOrderItems } from "../../../../../store/selectors"
 
 export function OrderComponentButtons() {
 
     const dispatch = useDispatch()
     const currentUser = useSelector(getCurrentUser)
     const orderItems = useSelector(getOrderItems)
+    const billItems = useSelector(getBillItems)
 
     const handleOrder = () => {
         const oi = [...orderItems]
         dispatch(saveAndPrintOrder(currentUser.name))
         dispatch(reduceStock(oi))
+    }
+    console.log(billItems)
+    const handleChargeBill = () => {
+        if (billItems.length) {
+            dispatch(chargeBill())
+        }
     }
 
     return (
@@ -22,7 +29,7 @@ export function OrderComponentButtons() {
                 <Button onClick={() => handleOrder()} className="w-100 py-3 mt-2" variant='outline-light'>Print &amp; Save</Button>
             </Col>
             <Col sm={6}>
-                <Button onClick={() => dispatch(chargeBill())} className="w-100 py-3 mt-2" variant='danger'>Charge</Button>
+                <Button onClick={() => handleChargeBill()} className="w-100 py-3 mt-2" variant='danger'>Charge</Button>
             </Col>
         </Row>
     )
