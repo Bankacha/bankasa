f9cd95901aff12413132bf9459be13592828e08e
Your Name <bgbrankoman@gmail.com>
2021-05-05 12:06:35 +0200

    implementing datepicker library and setting it up

--- src/admin/components/reports/Filter.js
+++ src/admin/components/reports/Filter.js
@@ -1,22 +1,39 @@
+import { useState } from "react";
 import { Col, Form, Row, Button } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export function Filter() {
+
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+    console.log(startDate)
+    console.log(endDate)
+
     return (
         <Col sm={12}>
             <Row className='d-flex align-items-center py-2 m-0 bg-secondary'>
-                <Col sm={3}>
-                    <Form.Control size='sm' as='select'>
-                        <option>Yesterday</option>
-                        <option>Today</option>
-                    </Form.Control>
+
+                <Col sm={4} className='d-flex align-items-center'>
+                    Start Date: &nbsp; <DatePicker
+                        className='bg-primary text-light'
+                        selected={startDate}
+                        onChange={date => setStartDate(date)}
+                        showTimeSelect
+                        dateFormat="Pp"
+                        isClearable />
+                </Col>
+                <Col sm={4} className='d-flex align-items-center'>
+                    End Date: &nbsp; <DatePicker
+                        className='bg-primary text-light'
+                        selected={endDate}
+                        onChange={date => setEndDate(date)}
+                        showTimeSelect
+                        dateFormat="Pp"
+                        isClearable />
                 </Col>
                 <Col sm={2} className='d-flex align-items-center'>
-                    From:<Form.Control size='sm' placeholder='25/3/2021 - 14:22'></Form.Control>
-                </Col>
-                <Col sm={2} className='d-flex align-items-center'>
-                    To: <Form.Control size='sm' placeholder='25/3/2021 - 18:46'></Form.Control>
-                </Col>
-                <Col sm={3} className='d-flex align-items-center'>
                     Waiter: <Form.Control size='sm' as='select'>
                         <option>All</option>
                         <option>Jeca</option>
@@ -25,7 +42,7 @@ export function Filter() {
                     </Form.Control>
                 </Col>
                 <Col sm={2}>
-                    <Button className='w-100' size='sm'>Show</Button>
+                    <Button className='w-100 bg-primary' size='sm'>Show</Button>
                 </Col>
             </Row>
         </Col>
